ad92d8cd004462d95258980bbe8f9a333c6d381d
Adam Sharp <adsharp@me.com>
2014-12-01 16:29:09 +1100

    1. Semantics (in Swift!)

---
+++ 1-semantics.swift
@@ -0,0 +1,137 @@
+import Darwin
+
+
+protocol Expression: Printable, DebugPrintable {
+  var reducible: Bool { get }
+  func reduce() -> Expression
+}
+
+
+// Numberber
+
+struct Number {
+  let value: Int
+}
+
+extension Number: Expression {
+  var description: String {
+    return "\(value)"
+  }
+
+  var debugDescription: String {
+    return "‹\(description)›"
+  }
+
+  var reducible: Bool {
+    return false
+  }
+
+  func reduce() -> Expression {
+    abort()
+  }
+}
+
+
+// Addition
+
+struct Add {
+  let left: Expression
+  let right: Expression
+}
+
+extension Add: Expression {
+  var description: String {
+    return "\(left.description) + \(right.description)"
+  }
+
+  var debugDescription: String {
+    return "‹\(description)›"
+  }
+
+  var reducible: Bool {
+    return true
+  }
+
+  func reduce() -> Expression {
+    switch (left.reducible, right.reducible) {
+    case (true, _):
+      return Add(left: left.reduce(), right: right)
+    case (_, true):
+      return Add(left: left, right: right.reduce())
+    default:
+      let l = left as? Number
+      let r = right as? Number
+      return Number(value: l!.value + r!.value)
+    }
+  }
+}
+
+
+// Multiply
+
+struct Multiply {
+  let left: Expression
+  let right: Expression
+}
+
+extension Multiply: Expression {
+  var description: String {
+    return "\(left.description) * \(right.description)"
+  }
+
+  var debugDescription: String {
+    return "‹\(description)›"
+  }
+
+  var reducible: Bool {
+    return true
+  }
+
+  func reduce() -> Expression {
+    switch (left.reducible, right.reducible) {
+    case (true, _):
+      return Add(left: left.reduce(), right: right)
+    case (_, true):
+      return Add(left: left, right: right.reduce())
+    default:
+      let l = left as? Number
+      let r = right as? Number
+      return Number(value: l!.value * r!.value)
+    }
+  }
+}
+
+
+// Machine
+
+struct Machine {
+  var expression: Expression
+
+  mutating func step() {
+    expression = expression.reduce()
+  }
+
+  mutating func run() {
+    while expression.reducible {
+      print()
+      step()
+    }
+    print()
+  }
+
+  func print() {
+    println(expression.debugDescription)
+  }
+}
+
+
+// main
+
+var machine = Machine(expression:
+                Add(
+                  left: Multiply(left: Number(value: 1), right: Number(value: 2)),
+                  right: Multiply(left: Number(value: 3), right: Number(value: 4))
+                )
+              )
+
+machine.run()
